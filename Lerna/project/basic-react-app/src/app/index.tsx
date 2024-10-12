e30c083d3483ed46d165baecebab4c308c49c9a1
HuilingDu <1720169376@qq.com>
2024-10-12 23:45:07 +0800

    feat: 新增 app 、Layout 组件

---
+++ Lerna/project/basic-react-app/src/app/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Layout/header";
+import Footer from "../components/Layout/footer";
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+};
+
+export default App;
---
+++ Lerna/project/basic-react-app/src/components/Layout/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+	return <div>这是底部</div>;
+};
+
+export default Footer;
